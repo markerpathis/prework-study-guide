d1ffb8ffc2a0dff88852000410b6cb77173bcf6e
Parker Mathis <ParkerMathis@Joannas-MacBook-Pro.local>
2022-08-28 14:14:47 -0700

    added notes to project

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,11 @@ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
+// Below is an explanation of each part of the for loop
+// for (expression 1; expression 2; expression 3)
+// Expression 1 is executed (one time) before the execution of the code block.
+// Expression 2 defines the condition for executing the code block.
+// Expression 3 is executed (every time) after the code block has been executed.
  for (var x = 0; x < topics.length; x++) {
    console.log(topics[x]);
  }
